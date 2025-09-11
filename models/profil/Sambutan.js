527d74f6dd8dca46f4bbf6fd8e7fd3348479dd41
haho88 <147403765+haho88@users.noreply.github.com>
2025-09-11 19:49:14 +0700

    Update Sambutan.js

--- models/profil/Sambutan.js
+++ models/profil/Sambutan.js
@@ -1,1 +1,13 @@
+const mongoose = require("mongoose");
+
+const sambutanSchema = new mongoose.Schema(
+  {
+    judul: { type: String, required: true },
+    isi: { type: String, required: true },
+    penulis: { type: String }, // misal Kepala Sekolah
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Sambutan", sambutanSchema);
 
