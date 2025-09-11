19fc291b1d80f5853878ecb1c7b508e148b09e86
haho88 <147403765+haho88@users.noreply.github.com>
2025-09-11 19:48:30 +0700

    Create Fasilitas.js

---
+++ models/profil/Fasilitas.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const fasilitasSchema = new mongoose.Schema(
+  {
+    nama: { type: String, required: true },
+    deskripsi: { type: String },
+    gambar: { type: String }, // opsional, simpan URL gambar
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Fasilitas", fasilitasSchema);
