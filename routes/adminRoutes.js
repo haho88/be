f458b174d7fa57a8eded6e1266a6907bbc478666
haho88 <147403765+haho88@users.noreply.github.com>
2025-09-12 15:22:30 +0700

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -8,7 +8,6 @@ import fs from "fs";
 
 import Admin from "../models/Admin.js";
 import Profil from "../models/Profil.js";
-import Guru from "../models/Guru.js";
 import Siswa from "../models/Siswa.js";
 import Alumni from "../models/Alumni.js";
 import Berita from "../models/Berita.js";
@@ -103,53 +102,6 @@ router.put("/profil/:id", auth, upload.single("image"), async (req, res) => {
 router.delete("/profil/:id", auth, async (req, res) => {
   await Profil.findByIdAndDelete(req.params.id);
   res.json({ message: "deleted" });
-});
-
-// ---------- Guru ----------
-router.get("/guru", async (req, res) => {
-  const items = await Guru.find().sort({ createdAt: -1 });
-  res.json(items);
-});
-
-router.post("/guru", auth, upload.single("foto"), async (req, res) => {
-  console.log("REQ BODY:", req.body);
-  console.log("REQ FILE:", req.file);
-  const obj = { ...req.body };
-  if (req.file) obj.foto = req.file.filename;
-  const doc = await Guru.create(obj);
-  res.json(doc);
-});
-
-router.put("/guru/:id", auth, upload.single("foto"), async (req, res) => {
-  const body = { ...req.body };
-  if (req.file) body.foto = req.file.filename;
-  const u = await Guru.findByIdAndUpdate(req.params.id, body, { new: true });
-  res.json(u);
-});
-
-router.delete("/guru/:id", auth, async (req, res) => {
-  try {
-    const guru = await Guru.findById(req.params.id);
-    if (!guru) {
-      return res.status(404).json({ message: "Guru tidak ditemukan" });
-    }
-
-    // Hapus file foto jika ada
-    if (guru.foto) {
-      const filePath = path.join(process.cwd(), "uploads", guru.foto);
-      if (fs.existsSync(filePath)) {
-        fs.unlinkSync(filePath);
-      }
-    }
-
-    // Hapus data dari DB
-    await Guru.findByIdAndDelete(req.params.id);
-
-    res.json({ message: "Data dan foto berhasil dihapus" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Gagal menghapus data guru" });
-  }
 });
 
 // ---------- Siswa ----------
