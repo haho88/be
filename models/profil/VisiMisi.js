ad1b1ba25e5d77cd08c22388839446bf43e8c6e9
haho88 <147403765+haho88@users.noreply.github.com>
2025-09-15 06:19:16 +0700

    Update VisiMisi.js

--- models/profil/VisiMisi.js
+++ models/profil/VisiMisi.js
@@ -1,10 +1,9 @@
-// models/profil/VisiMisi.js
 import mongoose from "mongoose";
 
 const visiMisiSchema = new mongoose.Schema(
   {
     visi: { type: String, required: true },
-    misi: [{ type: String }],
+    misi: { type: String, required: true },
   },
   { timestamps: true }
 );
