944cd976de306762fb7fdb1f8d53c47f2af25d26
haho88 <147403765+haho88@users.noreply.github.com>
2025-09-16 11:30:02 +0700

    Create Sejarah.js

---
+++ models/Sejarah.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const sejarahSchema = new mongoose.Schema(
+  {
+    isi: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Sejarah", sejarahSchema);
