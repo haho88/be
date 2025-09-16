3c9c051b484cdd68f352dc3776525051f1e796a6
haho88 <147403765+haho88@users.noreply.github.com>
2025-09-16 12:48:35 +0700

    Update VisiMisi.js

--- models/VisiMisi.js
+++ models/VisiMisi.js
@@ -1,11 +1,11 @@
 // models/VisiMisi.js
 import mongoose from "mongoose";
 
-const visimisiSchema = new mongoose.Schema({
+const VisiMisiSchema = new mongoose.Schema({
   nama: { type: String, required: true },
   deskripsi: { type: String },
   foto: { type: String, required: true  },
 }, { timestamps: true });
 
 
-export default mongoose.model("VisiMisi", visiMisiSchema);
+export default mongoose.model("VisiMisi", VisiMisiSchema);
