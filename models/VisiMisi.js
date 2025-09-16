4cffae979646c371a112ab063c497488b6a0311e
haho88 <147403765+haho88@users.noreply.github.com>
2025-09-16 12:32:59 +0700

    Update VisiMisi.js

--- models/VisiMisi.js
+++ models/VisiMisi.js
@@ -1,33 +1,11 @@
 // models/VisiMisi.js
 import mongoose from "mongoose";
 
-const visiMisiSchema = new mongoose.Schema(
-  {
-    visi: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    misi: {
-      type: [String], // Array of string (bisa banyak poin)
-      required: true,
-      validate: {
-        validator: function (arr) {
-          return arr.length > 0; // minimal ada 1 misi
-        },
-        message: "Misi harus memiliki minimal 1 poin.",
-      },
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-    updatedAt: {
-      type: Date,
-      default: Date.now,
-    },
-  },
-  { timestamps: true } // otomatis tambahkan createdAt & updatedAt
-);
+const VisiMisiSchema = new mongoose.Schema({
+  nama: { type: String, required: true },
+  deskripsi: { type: String },
+  foto: { type: String, required: true  },
+}, { timestamps: true });
+
 
 export default mongoose.model("VisiMisi", visiMisiSchema);
