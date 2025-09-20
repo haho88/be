29c8330568536e08329e4df1952325d8124deb14
haho88 <147403765+haho88@users.noreply.github.com>
2025-09-20 11:32:21 +0700

    Create PPDB_Formulir.js

---
+++ models/PPDB_Formulir.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const formulirPPDBSchema = new mongoose.Schema(
+  {
+    nama: { type: String, required: true },
+    nisn: { type: String, required: true },
+    alamat: { type: String, required: true },
+    asalSekolah: { type: String, required: true },
+    noHp: { type: String, required: true },
+    ijazah: { type: String }, // opsional, simpan filename/path hasil upload
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("FormulirPPDB", formulirPPDBSchema);
