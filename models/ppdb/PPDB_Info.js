623547b8c140222e494a50f892f6d118c8f78a3e
haho88 <147403765+haho88@users.noreply.github.com>
2025-09-20 08:04:29 +0700

    Update PPDB_Info.js

--- models/ppdb/PPDB_Info.js
+++ models/ppdb/PPDB_Info.js
@@ -1,1 +1,13 @@
+import mongoose from "mongoose";
+
+const ppdbInfoSchema = new mongoose.Schema(
+  {
+    tahunAjaran: { type: String, required: true }, // contoh: "2025/2026"
+    judul: { type: String, required: true },
+    deskripsi: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("PPDB_Info", ppdbInfoSchema);
 
