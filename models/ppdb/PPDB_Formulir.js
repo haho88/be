c7c375a5c99250dc46a2c3c0be08863a133bd3b1
haho88 <147403765+haho88@users.noreply.github.com>
2025-09-20 10:10:25 +0700

    Update PPDB_Formulir.js

--- models/ppdb/PPDB_Formulir.js
+++ models/ppdb/PPDB_Formulir.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const formulirPPDBSchema = new mongoose.Schema(
+const formulir_PPDBSchema = new mongoose.Schema(
   {
     nama: { type: String, required: true },
     nisn: { type: String, required: true },
@@ -12,4 +12,4 @@ const formulirPPDBSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("FormulirPPDB", formulirPPDBSchema);
+export default mongoose.model("Formulir_PPDB", formulir_PPDBSchema);
