f220a9ca3861637d3bfa4900ed40e61b865b878a
haho88 <147403765+haho88@users.noreply.github.com>
2025-09-24 18:40:54 +0700

    Update Fasilitas.js

--- models/Fasilitas.js
+++ models/Fasilitas.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const fasilitasSchema = new mongoose.Schema({
   nama: { type: String, required: true },
   deskripsi: { type: String },
-  foto: { type: String, required: true  },
+  foto: { type: String,}, // nama file lokal (fallback)
+  image: { type: String }, // URL Cloudinary
 }, { timestamps: true });
 
 export default mongoose.model("Fasilitas", fasilitasSchema);
